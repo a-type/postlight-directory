5a6cd1631580630c8ce6811f8927e46573221e66
Grant Forrest <gaforres@gmail.com>
2020-08-22 14:07:41 -0400

    feat: pagination of user list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
-import { Container, Box } from '@material-ui/core';
+import { Container, Box, Button } from '@material-ui/core';
 import { Navigation } from '../components/Navigation';
 import { gql, useQuery } from '@apollo/client';
 import { EmployeeGrid } from '../components/EmployeeGrid';
+import { useCallback } from 'react';
 
 const EmployeesQuery = gql`
-  query {
-    employees {
+  query Employees($take: Int, $skip: Int) {
+    employees(take: $take, skip: $skip) {
       id
       name
       profileImageUrl
@@ -15,14 +16,38 @@ const EmployeesQuery = gql`
   }
 `;
 export default function Home() {
-  const { data } = useQuery<{
+  const { data, fetchMore } = useQuery<{
     employees: {
       id: string;
       name: string;
       profileImageUrl: string;
       title: string;
     }[];
-  }>(EmployeesQuery);
+  }>(EmployeesQuery, {
+    variables: {
+      take: 25,
+      skip: 0,
+    },
+  });
+
+  const currentCount = data?.employees.length ?? 0;
+
+  const getNextPage = useCallback(() => {
+    fetchMore({
+      variables: {
+        take: 25,
+        skip: currentCount,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+
+        return {
+          ...prev,
+          employees: [...prev.employees, ...fetchMoreResult.employees],
+        };
+      },
+    });
+  }, [fetchMore, currentCount]);
 
   return (
     <>
@@ -32,7 +57,12 @@ export default function Home() {
       <Navigation />
       <Container maxWidth="lg">
         <Box pt={3}>
-          {data && <EmployeeGrid employees={data.employees ?? []} />}
+          {data && (
+            <>
+              <EmployeeGrid employees={data.employees ?? []} />
+              <Button onClick={getNextPage}>Show more</Button>
+            </>
+          )}
         </Box>
       </Container>
     </>
